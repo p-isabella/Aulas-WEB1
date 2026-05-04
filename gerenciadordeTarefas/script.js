2268dafd4784c5aaa4f5c74175e0efd5da84068e
Isabella <iisabellappessoa@gmail.com>
2026-05-04 11:11:22 -0300

    Add files via upload

---
+++ gerenciadordeTarefas/script.js
@@ -0,0 +1,6 @@
+let titulo = document.getElementById('tituloTarefa').innerHTML;
+let descricao = document.getElementById('DescricaoTarefa');
+let botaoAlta = document.getElementById("prioridadeAlta");
+let botaoMedia = document.getElementById("prioridadeMedia");
+let botaoBaixa = document.getElementById("prioridadeBaixa");
+
